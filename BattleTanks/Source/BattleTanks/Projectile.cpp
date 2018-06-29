9ed3f1d4a45d93e1802b7e31e242fad8c9ee27d1
DESKTOP-J1PKA4L\ahowe <ahowe2@att.net>
2018-06-29 15:33:39 -0400

    Projectile Fires

--- BattleTanks/Source/BattleTanks/Projectile.cpp
+++ BattleTanks/Source/BattleTanks/Projectile.cpp
@@ -1,6 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #include "Projectile.h"
+#include"BattleTanks.h"
+
+
 
 
 // Sets default values
@@ -8,6 +12,9 @@ AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	ProjectileMovement	= CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement"));
+	ProjectileMovement->bAutoActivate = false;
 
 }
 
@@ -23,5 +30,12 @@ void AProjectile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+void AProjectile::LaunchProjectile(float Speed)
+{
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f Projectile Fires at: %f"), Time, Speed);
+	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector *Speed);
+	ProjectileMovement->Activate();
 }
 
--- BattleTanks/Source/BattleTanks/Projectile.h
+++ BattleTanks/Source/BattleTanks/Projectile.h
@@ -2,8 +2,8 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "Classes/Engine/World.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 #include "Projectile.generated.h"
 
 UCLASS()
@@ -12,17 +12,18 @@ class BATTLETANKS_API AProjectile : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
+	
 	AProjectile();
 
-protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
+	void LaunchProjectile(float Speed);
+
+
+
+private:
+	UProjectileMovementComponent * ProjectileMovement = nullptr;
 	
 };
